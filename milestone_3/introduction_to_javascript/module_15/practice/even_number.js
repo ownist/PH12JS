cec4ad682b06e98b364612f408564ab206ced339
ownist <shahedevise@gmail.com>
2026-01-08 09:45:35 +0600

    feat(even-number): add for loop example for even numbers

--- milestone_3/introduction_to_javascript/module_15/practice/even_number.js
+++ milestone_3/introduction_to_javascript/module_15/practice/even_number.js
@@ -1,3 +1,4 @@
+// using while loop
 let num = 1;
 
 while (num <= 10) {
@@ -7,4 +8,13 @@ while (num <= 10) {
     console.log(`odd number: ${num}`);
   }
   num++;
+}
+
+console.log("\n"); // for create a new line
+
+// using for loop
+for (let i = 1; i < 11; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
