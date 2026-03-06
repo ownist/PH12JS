d1ec26bfbac21648f51e9225c858a825e6c2e77b
ownist <shahedevise@gmail.com>
2026-03-06 22:44:55 +0600

    chore(gpt-tasks): add string reverse solution

---
+++ milestone_4_problem_solving_with_javascript/module_20/AI_GIVEN_PRACTICE_TASKS/03_06_26_GPT_GIVEN_TASKS/08_task_eight.js
@@ -0,0 +1,13 @@
+/**
+ * String reverse করো
+ *
+ * ```md
+ * "javascript"
+ * ```
+ *
+ * Task:
+ * output হবে: "tpircsavaj"
+ */
+
+const str = "javascript";
+console.log(str.split("").reverse().join(""));
