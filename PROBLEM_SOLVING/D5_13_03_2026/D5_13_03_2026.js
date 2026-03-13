a37a31b67de1e916f8fab96a793615bf5ec690ec
ownist <shahedevise@gmail.com>
2026-03-13 20:37:11 +0600

    feat(palindrome): add improved check function
    - Implemented case-insensitive check
    - Used loop for efficient comparison
    - Added example usage for new function

--- PROBLEM_SOLVING/D5_13_03_2026/D5_13_03_2026.js
+++ PROBLEM_SOLVING/D5_13_03_2026/D5_13_03_2026.js
@@ -15,3 +15,21 @@ function isPalindrome(str) {
 const input = "madam";
 const output = isPalindrome(input);
 console.log(output);
+
+// for better logic
+const check_is_palindrome = (str) => {
+  const lower_str = str.toLowerCase();
+
+  for (let i = 0; i < lower_str.length / 2; i++) {
+    if (lower_str[i] !== lower_str[lower_str.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  // other ways return true
+  return true;
+};
+
+const input_str = "RaceCar";
+const result = check_is_palindrome(input_str);
+console.log(result);
