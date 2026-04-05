8282060d33c4025fa5f1c216d7be3e6b52365ba2
ownist <shahedevise@gmail.com>
2026-04-05 23:14:24 +0600

    feat(console): log variables with names smartly
    - Logged variables using object literal syntax
    - Improved readability of console output

---
+++ PROBLEM_SOLVING/lws(js_tips_and_tricks)_js_job_interview_questions/01/07.js
@@ -0,0 +1,10 @@
+// log values with variable names smartly
+const lib1 = "jQuery";
+const lib2 = "React";
+
+console.log(`library - 1: ${lib1}`);
+console.log(`library - 2 : ${lib2}`);
+
+// make it smart
+console.log({ lib1 });
+console.log({ lib2 });
