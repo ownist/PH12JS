1aecbcbaedc90b236417b09d9b47e874c001dfe8
ownist <shahedevise@gmail.com>
2026-04-05 22:30:04 +0600

    feat(js-tips): add array resizing example

---
+++ PROBLEM_SOLVING/lws(js_tips_and_tricks)_js_job_interview_questions/01/03.js
@@ -0,0 +1,10 @@
+// resizing an array
+let animals = ["kutta", "bilay", "murgi", "shiyal"];
+
+/**
+ * array er length property ke call kore nirdisto value set kore dile array ta sei nirdisto length er hoye jabe
+ *
+ * jodi emon kono value dei jeita array er total length theke boro tahole: puro array er modde jotogulo element ache sobgulo print korbe but jeigulo missing ache seigulor jonno nirdisto number onojayi empty items dekhabe. like: <4 empty items>
+ */
+animals.length = 2;
+console.log(animals);
