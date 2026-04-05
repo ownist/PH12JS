281814e9d7b4e221343548cb055f45d1dedf75a6
ownist <shahedevise@gmail.com>
2026-04-05 22:48:32 +0600

    feat(conditionals): add example for short conditionals
    - Demonstrates using && for short if statements
    - Shows using || for short if-else statements

---
+++ PROBLEM_SOLVING/lws(js_tips_and_tricks)_js_job_interview_questions/01/05.js
@@ -0,0 +1,18 @@
+// short conditionals
+let idol = "Tourist";
+
+// instead of doing this
+if (idol === "Tourist") {
+  console.log("salute");
+}
+
+// we can use && operator
+idol === "Tourist" && console.log("love emoji");
+
+// and instead of doing this
+if (idol !== "Tourist") {
+  console.log("angry emoji");
+}
+
+// we can use ||
+idol === "Tourist" || console.log("angry emoji");
