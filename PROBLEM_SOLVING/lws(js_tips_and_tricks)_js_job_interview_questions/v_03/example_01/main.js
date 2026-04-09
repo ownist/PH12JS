900432af12e9b9ae57a3fcadbea636bc7c4f5de1
ownist <shahedevise@gmail.com>
2026-04-09 22:53:43 +0600

    feat(example-01): add JS interview question 01
    - Introduce question on JS type coercion
    - Include expected output for console logs
    - Link `main.js` in html

---
+++ PROBLEM_SOLVING/lws(js_tips_and_tricks)_js_job_interview_questions/v_03/example_01/main.js
@@ -0,0 +1,6 @@
+/**
+ * question 01: what will be the output of the below code
+ */
+console.log([] + []); // output will be: ""
+console.log({} + []); // output will be: [object object]
+console.log(true + 5); // output will be: 6
