e8c35b520461be5c0e285077631db45b26485953
ownist <shahedevise@gmail.com>
2026-04-12 21:40:54 +0600

    recap: 20-6 Math, abs, round, ceil, floor, and random number

---
+++ milestone_4/problem_solving_with_javascript/module_20/javascript_simple_coding_problems_part_1/math.js
@@ -0,0 +1,31 @@
+// find minimum number
+const min = Math.min(45, 18, 2, 56, 4, 1);
+console.log(min);
+
+// find maximum number
+const max = Math.max(54, 85, 23, 4, 7);
+console.log(max);
+
+// PI number
+console.log(Math.PI);
+
+// abs -> eita kono value er absolute man dei.. i mean value  + - jeitai hok na keno eita poerator vanish kore dei. mane chinho soray fele. all time abs man dei
+console.log(Math.abs(2 - 10));
+
+// round
+console.log(Math.round(2.5)); // 3
+console.log(Math.round(4.45)); // 4
+console.log(Math.round(8.9)); // 9
+
+// floor (value doshomic ta bad diye purno songkha return korbe)
+console.log(Math.floor(2.95));
+console.log(Math.floor(80.99));
+
+// ceil (value tar tar opor er mna ta return korbe.. floor er oposit, floor niche namai and ceil opore uthay)
+console.log(Math.ceil(20.00001));
+console.log(Math.ceil(51.001));
+
+// random (0 theke 1 porjonto randomo number generate korbe but kono smy 1 return korbe nh)
+console.log(Math.random()); // 0 to 1
+console.log(Math.random() * 10); // 0 theke 10 er moddhe
+console.log(Math.round(Math.random() * 10)); // 0 theke 10 er moddhe purno songkha
