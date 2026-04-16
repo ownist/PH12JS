914cff1c9a2a886e2f7587a9e82ffce9e1f561ae
ownist <shahedevise@gmail.com>
2026-04-16 11:50:11 +0600

    solved: solve task_4 problem

---
+++ milestone_4/problem_solving_with_javascript/module_21/part_2_practice_tasks/task_4.js
@@ -0,0 +1,21 @@
+const phones = [
+  { model: "PhoneA", brand: "Iphone", price: 95000 },
+  { model: "PhoneB", brand: "Samsung", price: 40000 },
+  { model: "PhoneC", brand: "Oppo", price: 26000 },
+  { model: "PhoneD", brand: "Nokia", price: 35000 },
+  { model: "PhoneE", brand: "Iphone", price: 105000 },
+  { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+function findAveragePhonePrice(phones) {
+  let totalPrice = 0;
+
+  for (const p of phones) {
+    totalPrice += p.price;
+  }
+
+  return totalPrice / phones.length;
+}
+
+const avg = findAveragePhonePrice(phones);
+console.log(avg.toFixed(2));
