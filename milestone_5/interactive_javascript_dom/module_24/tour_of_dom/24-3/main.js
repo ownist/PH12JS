8e825aba4d7895bdf0770c8f53544dbaaeabcf87
ownist <shahedevise@gmail.com>
2026-04-19 23:59:20 +0600

    feat(dom): Add element selection practice
    - Implement getElementsByTagName example
    - Implement getElementById example
    - Implement getElementsByClassName example
    - Update index.html with new elements

--- milestone_5/interactive_javascript_dom/module_24/tour_of_dom/24-3/main.js
+++ milestone_5/interactive_javascript_dom/module_24/tour_of_dom/24-3/main.js
@@ -8,3 +8,34 @@ console.log(foreignKhabars);
 for (const e of foreignKhabars) {
   console.log(e);
 }
+
+console.log("\n");
+
+/**
+ * practice
+ */
+
+// getElementsByTagname
+const liTags = document.getElementsByTagName("li");
+console.log(liTags);
+for (const li of liTags) {
+  console.log(li);
+  console.log(li.innerText);
+}
+
+console.log("\n");
+
+// getElemetsById
+const para = document.getElementById("para");
+console.log(para);
+console.log(para.innerText);
+
+console.log("\n");
+
+// getElementsByClassName
+const navItems = document.getElementsByClassName("nav_item");
+console.log(navItems);
+
+for (const li of navItems) {
+  console.log(li);
+}
