ca02ebf7106199ebaf8c78b611ff3065bcc683da
ownist <shahedevise@gmail.com>
2026-05-05 00:04:50 +0600

    feat(problem): Add find object by ID template

---
+++ PROBLEM_SOLVING/ajaira_time_pass/F.js
@@ -0,0 +1,29 @@
+// Find Object by ID: একটি ইউজার লিস্ট (Array of Objects) থেকে নির্দিষ্ট একটি ID দিয়ে ওই ইউজারের পুরো তথ্য বের করো।
+
+function findUser(arr, id) {
+    let u = 
+  for (const user of arr) {
+    
+  }
+}
+
+const input = [
+  {
+    id: 1,
+    name: "ownist",
+  },
+  {
+    id: 2,
+    name: "jihad",
+  },
+  {
+    id: 3,
+    name: "rachel",
+  },
+  {
+    id: 4,
+    name: "tomjid",
+  },
+];
+const output = findUser(input, 2);
+console.log(output);
