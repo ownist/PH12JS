54b14ca9d83223ba9d9827225ef666c16e89c788
ownist <shahedevise@gmail.com>
2026-05-23 18:34:48 +0600

    feat: add string reversal solution

---
+++ PROBLEM_SOLVING/fic/01/sat_may_23_2026.js
@@ -0,0 +1,7 @@
+// rev a string
+function revStr(str) {
+  return `"${str}" er reverse version holo: "${str.split("").reverse().join("")}"`;
+}
+
+console.log(revStr("shahed"));
+console.log(revStr("ownist"));
