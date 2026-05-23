fa1d1079ca4b9643c32677191035d939d2ec4421
ownist <shahedevise@gmail.com>
2026-05-23 18:38:21 +0600

    feat: add largest number algorithm

--- PROBLEM_SOLVING/fic/01/sat_may_23_2026.js
+++ PROBLEM_SOLVING/fic/01/sat_may_23_2026.js
@@ -5,3 +5,18 @@ function revStr(str) {
 
 console.log(revStr("shahed"));
 console.log(revStr("ownist"));
+
+console.log("\n"); // for new empty line
+
+// find the largest number
+function largestNumber(arr) {
+  let num = arr[0];
+
+  for (const n of arr) {
+    if (n > num) num = n;
+  }
+
+  return `${arr} er moddhe largest number holo ${num}`;
+}
+
+console.log(largestNumber([2, 5, 8, 9, 6, 5, 8]));
