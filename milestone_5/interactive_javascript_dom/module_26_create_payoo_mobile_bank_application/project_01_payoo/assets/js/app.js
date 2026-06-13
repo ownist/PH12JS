2112076714b38d15abdf666d2d33d993d64b7da5
ownist <shahedevise@gmail.com>
2026-06-13 21:41:27 +0600

    recap: 26-3 Design Login Form (part-2 ) and add event listener

--- milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/project_01_payoo/assets/js/app.js
+++ milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/project_01_payoo/assets/js/app.js
@@ -1,1 +1,5 @@
-console.log("js file connected");
+// continue button
+document.getElementById("continue_btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(e.target);
+});
