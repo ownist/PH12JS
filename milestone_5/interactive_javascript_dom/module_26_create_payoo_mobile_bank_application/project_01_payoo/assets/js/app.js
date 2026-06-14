981835c08e992bebb1e4bea638a8abf2a517e2c9
ownist <shahedevise@gmail.com>
2026-06-14 23:45:07 +0600

    recap: 26-4 add login Functionality & Redirect to Home Page

--- milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/project_01_payoo/assets/js/app.js
+++ milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/project_01_payoo/assets/js/app.js
@@ -1,5 +1,24 @@
 // continue button
 document.getElementById("continue_btn").addEventListener("click", function (e) {
   e.preventDefault();
-  console.log(e.target);
+
+  // original phone numbers
+  const main_phone_number = "01912345678";
+  const main_four_digit_pin = "12345";
+
+  // user phone number
+  const user_phone_number = document.getElementById("user_mobile_number").value;
+  const user_four_digit_pin = document.getElementById(
+    "user_four_digit_pin",
+  ).value;
+
+  // condition
+  if (
+    user_phone_number === main_phone_number &&
+    user_four_digit_pin === main_four_digit_pin
+  ) {
+    window.location.href = "./dashboard.html";
+  } else {
+    alert("Invalid credentials!");
+  }
 });
