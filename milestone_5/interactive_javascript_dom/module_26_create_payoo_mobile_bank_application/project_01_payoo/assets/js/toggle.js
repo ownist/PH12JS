1a3cbaffa9985004056157a2920f00124b7af2ad
ownist <shahedevise@gmail.com>
2026-06-30 23:00:43 +0600

    feat(dashboard): implement view toggle logic
    - Add script to toggle between dashboard views
    - Add cash out section to dashboard HTML
    - Assign element IDs for DOM manipulation

---
+++ milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/project_01_payoo/assets/js/toggle.js
@@ -0,0 +1,4 @@
+document.getElementById("cash_out_card").addEventListener("click", function () {
+  document.getElementById("cash_out_section").style.display = "block";
+  document.getElementById("add_money_form").style.display = "none";
+});
