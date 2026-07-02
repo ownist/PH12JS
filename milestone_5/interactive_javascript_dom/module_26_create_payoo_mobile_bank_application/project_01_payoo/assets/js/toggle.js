a804c04c6ffa72c269be62576e7d6092404cecdb
ownist <shahedevise@gmail.com>
2026-07-02 22:06:56 +0600

    feat(dashboard): implement view toggling
    - 26-9 understand & Implement Toggle Feature
    - Allow toggling between money forms on dashboard
    - Align element IDs for consistent DOM targeting

--- milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/project_01_payoo/assets/js/toggle.js
+++ milestone_5/interactive_javascript_dom/module_26_create_payoo_mobile_bank_application/project_01_payoo/assets/js/toggle.js
@@ -1,4 +1,13 @@
+// add money card
+document
+  .getElementById("add_money_card")
+  .addEventListener("click", function () {
+    document.getElementById("add_money_form_parent").style.display = "block";
+    document.getElementById("cash_out_parent").style.display = "none";
+  });
+
+// cash out card
 document.getElementById("cash_out_card").addEventListener("click", function () {
-  document.getElementById("cash_out_section").style.display = "block";
-  document.getElementById("add_money_form").style.display = "none";
+  document.getElementById("cash_out_parent").style.display = "block";
+  document.getElementById("add_money_form_parent").style.display = "none";
 });
